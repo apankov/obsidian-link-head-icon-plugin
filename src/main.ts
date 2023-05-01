d563bc9da0f6c8d60d8318fedf64964ad5e89ac5
Andrey Pankov <a.pankov@gmail.com>
2023-05-01 14:10:16 +0200

    feat: adding mode websites

--- src/main.ts
+++ src/main.ts
@@ -3,11 +3,17 @@ import { getAssetsPathResolver } from './util';
 
 const WEBSITES: Record<string, string> = {
 	'//docs.google.com/spreadsheets/': 'spreadsheets.png',
-	'//docs.google.com/document/': 'google-drive.png',
+	'//docs.google.com/document/': 'google-docs.png',
 	'//drive.google.com/drive/': 'google-drive.png',
 	'youtube.com/watch': 'youtube.png',
+	'//www.youtube.com/': 'youtube.png',
+	'//youtu.be/': 'youtube.png',
 	'//twitter.com/': 'twitter.png',
-	'//gitlab.com/': 'twitter.png',
+	'//gitlab.com/': 'gitlab.png',
+	'//www.facebook.com/': 'facebook.png',
+	'//github.com/': 'github-light.png',
+	'asana.com/': 'asana.png',
+	'//www.npmjs.com/': 'npmjs.png',
 };
 
 class LinkIcon extends MarkdownRenderChild {
@@ -23,7 +29,7 @@ class LinkIcon extends MarkdownRenderChild {
 
 	onload() {
 		const icon = this.containerEl.createEl('div', { attr: {
-			class: 'external-link-wrapper',
+			class: 'external-link-icon',
 			style: 'background-image: url("'+this.resolver(this.website)+'");',
 		}});
 
@@ -39,7 +45,7 @@ export default class LinkHeadIconPlugin extends Plugin {
 			const resolver = getAssetsPathResolver(this, 'assets');
 
 			for (let index = 0; index < links.length; index++) {
-				const link = links.item(index);
+				const link = links.item(index) as HTMLElement;
 				const text = link.innerText.trim();
 
 				for (const pattern in WEBSITES) {
